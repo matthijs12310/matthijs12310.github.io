eec25a8318ecc49135c5b62f308a6b5e1e4db36b
matthijs12310 <bobstoffer1596@gmail.com>
2021-03-25 12:48:18 +0100

    fr

---
+++ cool.js
@@ -0,0 +1,10 @@
+var d = new Date();
+      var n = d.getHours();
+      
+      if (n > 19 || n < 6)
+        $("body").css("background", "url('night.jpg')");
+      else if (n > 16 && n < 19)
+        $("body").css("background", "url('sunset.jpg')");
+      else
+         document.getElementById("Text1").innerHTML = "Het is " + n + " uur";
+        $("body").css("background-image", "url('https://www.almanac.com/sites/default/files/styles/primary_image_in_article/public/image_nodes/summer-sunset.jpg')");
