3801b60ed1091eac419bc38e7aca7e2d83ada276
matthijs12310 <bobstoffer1596@gmail.com>
2021-03-28 22:51:40 +0200

    fjh

--- cool.js
+++ cool.js
@@ -1,10 +1,34 @@
-var d = new Date();
+function checkImage() {
+    var d = new Date();
       var n = d.getHours();
       
       if (n > 19 || n < 6)
-        $("body").css("background", "url('night.jpg')");
-      else if (n > 16 && n < 19)
-        $("body").css("background", "url('sunset.jpg')");
+        $("body").css("background", "url('avond.jpg')");
+      else if (n > 13 && n < 19)
+        $("body").css("background", "url('middag.jpg')");
       else
-         document.getElementById("Text1").innerHTML = "Het is " + n + " uur";
-        $("body").css("background-image", "url('https://www.almanac.com/sites/default/files/styles/primary_image_in_article/public/image_nodes/summer-sunset.jpg')");
+         $("body").css("background-image", "url('https://www.tsbouwvastgoed.nl/wp-content/uploads/2020/07/chuttersnap-TSgwbumanuE-unsplash-2000x1050.jpg')");
+}
+function checkTime(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
+  checkImage()
+  function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    // add a zero in front of numbers<10
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+    t = setTimeout(function() {
+      startTime()
+      checkImage()
+    }, 500);
+  }
+  startTime();
+  console.log("lol");
