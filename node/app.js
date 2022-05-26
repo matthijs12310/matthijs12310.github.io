d04589599fe798a8e10f01e2d7a8d2cb6f05858b
Matthijs Stoffer <63472519+matthijs12310@users.noreply.github.com>
2022-05-26 14:22:15 +0000

    asdf

--- node/app.js
+++ node/app.js
@@ -4,5 +4,6 @@ const fs = require('fs');
 
 http.createServer(function (req, res) {
   res.writeHead(200);
-  res.end("hello world\n");
+  res.end("This is only setup for the mc server :)");
+  console.log("WOWOWOWOWWO");
 }).listen(8080);
