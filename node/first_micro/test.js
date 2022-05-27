813f9b4931685829d1b7ebf21bdad544661cad02
Matthijs Stoffer <63472519+matthijs12310@users.noreply.github.com>
2022-05-27 18:50:11 +0000

    hj

--- node/first_micro/test.js
+++ node/first_micro/test.js
@@ -1,5 +1,21 @@
-const greet = (name) => {
-    console.log(`hello, ${name}`);
-}
+const http = require("http");
+const fs = require('fs');
 
-greet("marui");
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+    res.setHeader('Content-Type', 'text/html');
+    fs.readFile('./index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        }
+        else{
+            res.write(data);
+            res.end();
+        }
+    });
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log("It works");
+});
