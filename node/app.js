93dad83bc022993c5f1e09b84cb3f201e8e15660
Matthijs Stoffer <63472519+matthijs12310@users.noreply.github.com>
2022-05-27 09:42:20 +0000

    poepje

--- node/app.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const fs = require('fs');
-
-
-http.createServer(function (req, res) {
-  res.writeHead(200);
-  res.end("This is only setup for the mc server :)");
-  console.log("WOWOWOWOWWO");
-}).listen(8080);
