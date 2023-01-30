ae6c6656ee1dfa91b3edf562261f341e761d85f3
Matthijs Stoffer <63472519+matthijs12310@users.noreply.github.com>
2023-01-30 16:56:30 +0100

    Update pcsetup.sh

--- pcsetup.sh
+++ pcsetup.sh
@@ -1,10 +1,15 @@
 echo Matta PC dinges V0.1
 echo
+cd /
 echo Installing Ventoy...
 wget https://github.com/ventoy/Ventoy/releases/download/v1.0.88/ventoy-1.0.88-linux.tar.gz
 tar -xzf ventoy-1.0.88-linux.tar.gz
 cd ventoy-1.0.88
 sh Ventoy2Disk.sh -i -g /dev/vdb
 echo
+cd ..
+wget thijs.tk/pcsetup2.sh
+mv pcsetup2.sh /etc/init.d/
+chmod +x /etc/init.d/pcsetup2.sh
 echo Installing Win10Fix.iso...
 wget https://dl.dropboxusercontent.com/s/0pk3m8r2svisf8k/Win10fix.iso
